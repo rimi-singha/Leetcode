ee4dc2cb794941b89bcf9040f65c73776407d196
Rimi Singha <rimisingha86@gmail.com>
2026-08-09 00:02:48 +0530

    Time: 1 ms (97.52%), Space: 47.4 MB (60.53%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,42 +1,19 @@
-/*class Solution {
-    public int pivotIndex(int[] nums) {
-        int totalsum=0;
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-             totalsum=totalsum+nums[i];
-        }
-         for(int i=1;i<nums.length;i++){
-            int leftsum=sum+nums[i-1];
-             int rightsum = totalSum - leftsum - nums[i];
-         }
-         if(leftsum==rightsum){
-                     return i;
-                }
-            }
-    }*/
-          class Solution {
-    public int pivotIndex(int[] nums) {
-
+        class Solution {
+        public int pivotIndex(int[] nums) {
         int totalsum = 0;
         int leftsum = 0;
-
         // Find total sum
         for (int i = 0; i < nums.length; i++) {
             totalsum += nums[i];
         }
-
         // Check every possible pivot
         for (int i = 0; i < nums.length; i++) {
-
             int rightsum = totalsum - leftsum - nums[i];
-
             if (leftsum == rightsum) {
-                return i;
+                return i;         //return pivot index
             }
-
             leftsum += nums[i];
         }
-
         return -1;
     }
 }
