0ae72f68355de8e7b28eb1ae81c2c85ece57b0f0
Rimi Singha <rimisingha86@gmail.com>
2026-08-23 20:10:18 +0530

    Time: 0 ms (100%), Space: 47.9 MB (44.55%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -5,29 +5,23 @@ class Solution {
         int water = 0;
 
         while (left < right) {
-
             if (height[left] <= height[right]) {
-
                 if (height[left] >= leftMax) {
                     leftMax = height[left];
                 } else {
                     water += leftMax - height[left];
                 }
-
                 left++;
 
             } else {
-
                 if (height[right] >= rightMax) {
                     rightMax = height[right];
                 } else {
                     water += rightMax - height[right];
                 }
-
                 right--;
             }
         }
-
         return water;
     }
 }
