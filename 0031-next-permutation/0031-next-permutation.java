13790e419aca8bdc4d07bed3f21505065c684b46
Rimi Singha <rimisingha86@gmail.com>
2026-09-11 00:00:01 +0530

    Time: 0 ms (100%), Space: 44.6 MB (51.28%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,12 +2,10 @@ class Solution {
     public void nextPermutation(int[] nums) {
 
         int i = nums.length - 2;
-
         // 1. Find the first decreasing element
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
-
         // 2. Find the element just larger than nums[i]
         if (i >= 0) {
             int j = nums.length - 1;
